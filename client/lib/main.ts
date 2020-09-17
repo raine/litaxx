bc56c0690bb6459c77aa5f9348bfa28b138fd802
Raine Virta <raine.virta@gmail.com>
2020-09-17 11:44:01 +0300

    asdf

--- client/lib/main.ts
+++ client/lib/main.ts
@@ -1,10 +1,14 @@
-const socket = io()
+import { Board } from './board'
+
+console.log(Board)
+
+// const socket = io()
 
 const new_game = document.querySelector('#create-a-game')
 
 new_game.addEventListener('submit', event => {
     event.preventDefault()
-    socket.emit('new_game')
+    // socket.emit('new_game')
 })
 
 const join_game = document.querySelector('#join-a-game')
@@ -12,12 +16,12 @@ const game_id = document.querySelector('#game-id')
 
 join_game.addEventListener('submit', event => {
     event.preventDefault()
-    socket.emit('join_game', game_id.value)
+    // socket.emit('join_game', game_id.value)
 })
 
-socket.on('chat', message => {
-    console.log('From server: ', message)
-})
+/* socket.on('chat', message => { */
+/*     console.log('From server: ', message) */
+/* }) */
 
 function clicked(id) {
     const square = parseInt(id.charAt(1));
